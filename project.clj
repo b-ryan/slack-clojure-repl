91a7cf4544311745cd14ed27b345254bd2e5dbc5
Buck Ryan <fryan@rjmetrics.com>
2014-09-23 21:27:40 -0400

    try to make project.clj mimick heroku version

--- project.clj
+++ project.clj
@@ -7,9 +7,12 @@
                  [compojure "1.1.8"]
                  [clj-http "1.0.0"]
                  [environ "0.5.0"]]
-  :plugins [[lein-ring "0.8.11"]]
+  :plugins [[lein-ring "0.8.11"]
+            [environ/environ.lein "0.2.1"]]
   :ring {:handler slack-clojure-repl.handler/app}
   :uberjar-name "slack-clojure-repl-standalone.jar"
+  :hooks  [environ.leiningen.hooks]
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}
+   :production {:env {:production true}}})
